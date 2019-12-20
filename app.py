2b49eff6c8bf2e1fc99bb0d6be7d2ff683ab27df
Ankit Kumar <nktkumar82@gmail.com>
2019-12-20 13:27:50 +0530

    Twitter Bot Login page opening

---
+++ app.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class TwitterBot:
+     def __init__(self,username,password):
+         self.username = username
+         self.password = password
+         self.bot = webdriver.Firefox()
+
+     def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(3)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+
+ak = TwitterBot("nktkumar82@gmail.com","Ankit@1234")
+ak.login()
